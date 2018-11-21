5299c783beb5350dfa6314a72dfcf112cf1e9de9
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2018-11-21 22:08:45 +0000

    Added weatherrequest function - Calls weather API with lat lon coords

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
+const weatherRequest = require('./weatherRequest/weatherRequest');
 
 const argv = yargs
   .options({
@@ -14,7 +15,15 @@ const argv = yargs
   .help()
   .alias('help', 'h').argv;
 
-geocode.geocodeAddress(argv.a, (err, res) => {
+// geocode.geocodeAddress(argv.a, (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(JSON.stringify(res, undefined, 2));
+//   }
+// });
+
+weatherRequest.getWeather(51.5072, 0.1275, (err, res) => {
   if (err) {
     console.log(err);
   } else {
